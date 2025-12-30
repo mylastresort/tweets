626cbf0b3d6d7fff11143b85a9fc0a4e42467401
mylastresort <samytamim0@gmail.com>
2025-12-30 19:25:17 +0100

    refactor: using stratification to split data and generating datasets to train models with splitted data

--- train.py
+++ train.py
@@ -1,28 +1,32 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
 from vectorizer import count_vectorize
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 from cleaning import clean_tweets
 
-def train_model(
-        df,
-        tweet_column = 'tweet',
-        sentiment_column = 'sentiment',
-        cleaner = clean_tweets,
-        tokenizer = None,
-        vectorizer = count_vectorize,
-        classifier = RandomForestClassifier(n_estimators=100, random_state=42)):
-    vectorizer, bow_matrix = vectorizer(df, tweet_column, cleaner=cleaner, tokenizer=tokenizer)
-    x_train, x_test, y_train, y_test = train_test_split(
-        bow_matrix,
-        df[sentiment_column],
-        test_size=0.2,
-        random_state=42
+
+def generate_test_bow_matrix(
+    df,
+    tweet_column="tweet",
+    cleaner=clean_tweets,
+    tokenizer=None,
+    vectorizer=count_vectorize,
+):
+    vectorizer, bow_matrix = vectorizer(
+        df, tweet_column, cleaner=cleaner, tokenizer=tokenizer
     )
 
+    return bow_matrix
+
+
+def train_model(
+    x_train,
+    y_train,
+    classifier=RandomForestClassifier(n_estimators=100, random_state=42),
+):
     classifier.fit(x_train, y_train)
-    return classifier, vectorizer, x_test, y_test
+    return classifier
+
 
 def evaluate_model(model, x_test, y_test, quiet=True):
     test_predictions = model.predict(x_test)
--- vectorizer.py
+++ vectorizer.py
@@ -3,8 +3,10 @@ from tokenizer import tokenize_tweet
 from cleaning import clean_tweets
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+
 def custom_tokenizer(method=None):
     """Custom tokenizer that returns pre-tokenized input."""
+
     def cb(text):
         tokenized_text = tokenize_tweet(text)
         if method is None:
@@ -13,17 +15,41 @@ def custom_tokenizer(method=None):
 
     return cb
 
-def count_vectorize(tweets, text_column, cleaner = clean_tweets, tokenizer=None, binary=False):
+
+def count_vectorize(
+    tweets, text_column, cleaner=clean_tweets, tokenizer=None, binary=False
+):
     """Create a Bag of Words representation of the cleaned tweets in the dataframe."""
-    vectorizer = CountVectorizer(tokenizer=custom_tokenizer(method=tokenizer), binary=binary)
-    bow_matrix = vectorizer.fit_transform(cleaner(tweets, text_column=text_column)[text_column])
+    vectorizer = CountVectorizer(
+        tokenizer=custom_tokenizer(method=tokenizer), binary=binary
+    )
+    bow_matrix = vectorizer.fit_transform(
+        cleaner(tweets, text_column=text_column)[text_column]
+    )
     return vectorizer, bow_matrix
 
-def binary_vectorize(tweets, text_column, cleaner = clean_tweets, tokenizer=None):
+
+def binary_vectorize(tweets, text_column, cleaner=clean_tweets, tokenizer=None):
     return count_vectorize(tweets, text_column, cleaner, tokenizer, binary=True)
 
-def tfidf_vectorize(tweets, text_column, cleaner = clean_tweets, tokenizer=None):
+
+def tfidf_vectorize(tweets, text_column, cleaner=clean_tweets, tokenizer=None):
     """Create a TF-IDF representation of the cleaned tweets in the dataframe."""
     vectorizer = TfidfVectorizer(tokenizer=custom_tokenizer(method=tokenizer))
-    tfidf_matrix = vectorizer.fit_transform(cleaner(tweets, text_column=text_column)[text_column])
+    tfidf_matrix = vectorizer.fit_transform(
+        cleaner(tweets, text_column=text_column)[text_column]
+    )
     return vectorizer, tfidf_matrix
+
+
+def vectorizer_transform(
+    tweets,
+    vectorizer,
+    text_column,
+    cleaner=clean_tweets,
+):
+    """Transform new tweets using an existing vectorizer."""
+    transformed_matrix = vectorizer.transform(
+        cleaner(tweets, text_column=text_column)[text_column]
+    )
+    return transformed_matrix
